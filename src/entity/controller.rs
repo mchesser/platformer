fbe69b2043f57122ae0d77f276397369f9be97f1
quvarxa <michaelchesser@outlook.com>
2014-02-15 20:17:44 +1030

    Fix too long lines

--- src/entity/controller.rs
+++ src/entity/controller.rs
@@ -22,12 +22,13 @@ impl<T: Entity> Controller for Player<T> {
     fn update(&mut self, map: &Map, secs: f32) {
         let keyboard = self.keyboard.borrow().borrow();
 
+        let move_accel = self.entity.physical_properties().movement_accel;
         let x_accel =
             if keyboard.get().is_keydown(keycode::LeftKey) {
-                -self.entity.physical_properties().movement_accel * if self.entity.is_on_ground() { 1.0 } else { 0.6 }
+                -move_accel * if self.entity.is_on_ground() { 1.0 } else { 0.6 }
             }
             else if keyboard.get().is_keydown(keycode::RightKey) {
-                self.entity.physical_properties().movement_accel * if self.entity.is_on_ground() { 1.0 } else { 0.6 }
+                move_accel * if self.entity.is_on_ground() { 1.0 } else { 0.6 }
             }
             else {
                 0.0
@@ -35,8 +36,9 @@ impl<T: Entity> Controller for Player<T> {
         let new_accel = Vec2::new(x_accel, self.entity.acceleration().y);
         self.entity.set_acceleration(new_accel);
 
+        let jump_accel = self.entity.physical_properties().jump_accel;
         if self.entity.is_on_ground() && keyboard.get().is_new_keypress(keycode::UpKey) {
-            let new_velocity = self.entity.velocity() + Vec2::new(0.0, -self.entity.physical_properties().jump_accel);
+            let new_velocity = self.entity.velocity() + Vec2::new(0.0, -jump_accel);
             self.entity.set_velocity(new_velocity);
         }
 
